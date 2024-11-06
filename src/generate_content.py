68673fe7c495bc3364688f29909baabdf15c8d80
t3hsquirr3l <naustin3.6@gmail.com>
2024-11-06 15:30:12 -0700

    Added recursive page generation for all .md files in the content directory

--- src/generate_content.py
+++ src/generate_content.py
@@ -1,9 +1,8 @@
 from html_blocks import *
 import os
+import pathlib
 
 def generate_page(from_path, template_path, dest_path):
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
-
     with open(from_path, encoding="utf-8") as markdown_file:
         markdown = markdown_file.read()
     # print(f"{from_path} closed: {markdown_file.closed}")
@@ -26,6 +25,28 @@ def generate_page(from_path, template_path, dest_path):
         f.write(final_page)
     # print(f"{dest_path} closed: {f.closed}")
     
+def generate_pages_recursive(content_path, template_path, destination_path):
+    content_dir = os.listdir(content_path)
+    print(f"destination: {destination_path}")
+    print(f"dir {content_path}: {content_dir}")
+    for filename in content_dir:
+        file_path = pathlib.Path(content_path, filename)
+        print(f"full path: {file_path}")
+
+        if os.path.isfile(file_path) and file_path.suffix == ".md":
+            print(f"markdown file: {filename}")
+            page_path = pathlib.Path(destination_path, filename).with_suffix(".html")
+            print(f"destination: {page_path}")
+            generate_page(file_path, template_path, page_path)
+
+        elif os.path.isdir(file_path):
+            print(f"folder: {filename}")
+            destination_path = os.path.join(destination_path, filename)
+            print(f"new destination: {destination_path}")
+            generate_pages_recursive(file_path, template_path, destination_path)
+
+
+
 def extract_title(markdown):
     blocks = markdown_to_blocks(markdown)
     if not blocks[0].startswith("# "):
--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from textnode import TextNode, TextType
 from copystatic import copy_over_directory
-from generate_content import generate_page
+from generate_content import generate_page, generate_pages_recursive
 import os
 import shutil
 
 static_path = "./static"
 public_path = "./public"
 
-content_path = "./src/content/index.md"
+content_path = "./src/content"
 template_path = "template.html"
 
 def main():
@@ -18,6 +18,9 @@ def main():
     print(f"copying files from static directory {static_path} to public directory {public_path}")
     copy_over_directory(static_path, public_path)
     
-    generate_page(content_path, template_path, "public/index.html")
+    print(f"Generating pages from {content_path} using {template_path} -> {public_path}")
+    # print(f"Generating page from {content_path} using{template_path} -> {public_path}")
+    # generate_page(content_path, template_path, "public/index.html")
+    generate_pages_recursive(content_path, template_path, public_path)
 
 main()
